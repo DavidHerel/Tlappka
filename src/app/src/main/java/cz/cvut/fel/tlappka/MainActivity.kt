4c933bba42c908f1b691d0f582b33ca9c1c3c584
Nela Petrzelkova <nela.petrzelkova@seznam.cz>
2020-04-17 18:01:43 +0200

    added toolbar and navigation to main activity

--- src/app/src/main/java/cz/cvut/fel/tlappka/MainActivity.kt
+++ src/app/src/main/java/cz/cvut/fel/tlappka/MainActivity.kt
@@ -2,11 +2,23 @@ package cz.cvut.fel.tlappka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toolbar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.home_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setTitle("Novinky")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
     }
 }
