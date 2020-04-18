39332e593adb53b68906f1684b7f45a5d57987f3
Daniela Hradilova <hradidan@fel.cvut.cz>
2020-04-18 19:33:33 +0200

    Basic navigation structure and handeling

--- src/app/src/main/java/cz/cvut/fel/tlappka/MainActivity.kt
+++ src/app/src/main/java/cz/cvut/fel/tlappka/MainActivity.kt
@@ -4,16 +4,62 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.setContentView
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import cz.cvut.fel.tlappka.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {item->
+        when(item.itemId) {
+            R.id.nav_home -> {
+                replaceFragment(HomeFragment())
+                supportActionBar?.setTitle("Novinky")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_profile -> {
+                replaceFragment(ProfileFragment())
+                supportActionBar?.setTitle("Profil")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_events -> {
+                replaceFragment(EventFragment())
+                supportActionBar?.setTitle("Události")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_friends -> {
+                replaceFragment(FriendsFragment())
+                supportActionBar?.setTitle("Přátelé")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_notification -> {
+                replaceFragment(NotificationsFragment())
+                supportActionBar?.setTitle("Oznámení")
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        return@OnNavigationItemSelectedListener false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = setContentView(this, R.layout.activity_main)
+
+        // TODO: Replace findViewById() with binding object handle - error not Toolbar object
+        /*val toolbar: Toolbar = binding.homeToolbar
+        setSupportActionBar(binding.homeToolbar)
+        supportActionBar?.setTitle("Novinky")*/
         val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.home_toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.setTitle("Novinky")
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        replaceFragment(HomeFragment())
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -21,4 +67,11 @@ class MainActivity : AppCompatActivity() {
         inflater.inflate(R.menu.menu, menu)
         return true
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
