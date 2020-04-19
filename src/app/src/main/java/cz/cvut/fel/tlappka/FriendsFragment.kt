d0a6ebeb6b3e294145650429cabff0efa67d3cc4
Nela Petrzelkova <nela.petrzelkova@seznam.cz>
2020-04-19 15:19:16 +0200

    added functionality to option items

--- src/app/src/main/java/cz/cvut/fel/tlappka/FriendsFragment.kt
+++ src/app/src/main/java/cz/cvut/fel/tlappka/FriendsFragment.kt
@@ -3,8 +3,11 @@ package cz.cvut.fel.tlappka
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 /**
  * A simple [Fragment] subclass.
