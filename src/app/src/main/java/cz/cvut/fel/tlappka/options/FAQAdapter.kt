cdc617f30664c33b95e1774b54f5a676b25833ff
Nela Petrzelkova <nela.petrzelkova@seznam.cz>
2020-05-01 12:46:54 +0200

    Added edit password layout

--- src/app/src/main/java/cz/cvut/fel/tlappka/options/FAQAdapter.kt
+++ src/app/src/main/java/cz/cvut/fel/tlappka/options/FAQAdapter.kt
@@ -1,18 +1,15 @@
 package cz.cvut.fel.tlappka.options
 
-import android.content.Context
-import android.graphics.ColorSpace
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import cz.cvut.fel.tlappka.R
 import kotlinx.android.synthetic.main.faq_item.view.*
 
-class FAQAdapter(private val FAQList: List<FAQItem>) :
-    RecyclerView.Adapter<FAQAdapter.FAQViewHolder>() {
+class ListAdapter(private val listFAQ: List<FAQItem>) :
+    RecyclerView.Adapter<ListAdapter.FAQViewHolder>() {
 
     class FAQViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
         val questionView : TextView = itemView.question
@@ -25,11 +22,11 @@ class FAQAdapter(private val FAQList: List<FAQItem>) :
     }
 
     override fun getItemCount(): Int {
-        return FAQList.size
+        return listFAQ.size
     }
 
     override fun onBindViewHolder(holder: FAQViewHolder, position: Int) {
-        val currentItem = FAQList[position]
+        val currentItem = listFAQ[position]
         holder.questionView.text = currentItem.q
         holder.answerView.text = currentItem.a
     }
